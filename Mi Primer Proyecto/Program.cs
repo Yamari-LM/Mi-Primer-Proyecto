4495ddd388afc7db92629867ffcc88438cfcf777
Yamari-LM <lisethvelasquez318@gmail.com>
2024-08-15 13:59:23 -0600

    actualizar cambios de codigo

--- Mi Primer Proyecto/Program.cs
+++ Mi Primer Proyecto/Program.cs
@@ -1,12 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-0{
-    Console.WriteLine("Bienvenidos a nuestro primer sistema creado por github");
+﻿using System;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenidos a nuestro primer sistema creado por github");
 
-    Console.WriteLine("Ingrese su nombre para continuar");
-    string Nombre = Console.ReadLine(); //Para recibir valores de caracter
-    Console.WriteLine("Ingrese su edad");
-    int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
-    double EdadMeses = Edad * 12; //Calculos Matematicos
-    Console.WriteLine(Nombre + "Tu edad en Meses es:" + EdadMeses + "Meses");//concatenacion
-
-}
+        Console.WriteLine("Ingrese su nombre para continuar");
+        string Nombre = Console.ReadLine(); //Para recibir valores de caracter
+        Console.WriteLine("Ingrese su edad");
+        int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
+        double EdadMeses = Edad * 12; //Calculos Matematicos
+        Console.WriteLine(Nombre + "Tu edad en Meses es:" + EdadMeses + "Meses");//concatenacion} 
